1af7ba6616739985facb995743e58ccd1b4eaa8f
aqhillaqhoriza <aqhillaamour@gmail.com>
2022-05-13 10:19:23 +0700

    materi

---
+++ android/app/src/main/kotlin/com/example/materi6/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.materi6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MaterialApp(
+    title: "Belajar Form Flutter",
+    home: BelajarForm(),
+  ));
+}
+class BelajarForm extends StatefulWidget {
+  @override
+  _BelajarFormState createState() => _BelajarFormState();
+}
+class _BelajarFormState extends State<BelajarForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BelajarFlutterHariIni"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+             // tambahkan komponen seperti input field disini
+             TextFormField(
+      decoration: new InputDecoration(
+      hintText: "Masukkan nama ingame",
+      labelText: "Nama Ingame",
+      icon: Icon(Icons.people)),
+)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
